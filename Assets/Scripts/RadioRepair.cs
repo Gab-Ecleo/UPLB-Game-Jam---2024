58d71a39471488240d7ebc9ce339258d31e41eec
Ragaliah <loller231@yahoo.com>
2024-05-19 21:48:49 +0800

    Added audio for repair

--- Assets/Scripts/RadioRepair.cs
+++ Assets/Scripts/RadioRepair.cs
@@ -15,7 +15,8 @@ public class RadioRepair : MonoBehaviour, IInteractable
         //Replace Radio Spitesheet
         //Display dialog
         //Itirate Sequence
-        
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.radioStatic, this.transform.position);
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.fixingRadio, this.transform.position);
         Debug.Log("Fixing Radio");
         GameManager.Instance.CanFixRadio = false;
     }

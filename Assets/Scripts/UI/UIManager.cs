065f089e4cf13144d727dfb2cc2df00fe054e6bc
Ty <73094402+tynics@users.noreply.github.com>
2024-08-04 16:20:22 +0800

    new pause screen anim

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,25 +4,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Player hud")]
     [SerializeField] private Image currentOxygen;
     [SerializeField] private Image currentFood;
-
     [SerializeField] private TMP_Text foodText;
     [SerializeField] private TMP_Text oxygenText;
 
+    [Header("Pause Menu")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private bool isPaused;
+
+    [Header("Tween Animation")]
+    [SerializeField] private GameObject pausePanelGO;
+    [SerializeField] private CanvasGroup pausefadePanel;
+    [SerializeField] private float topY;
+    [SerializeField] private float midY;
+    [SerializeField, Range(0,1)] private float tweenDuration;
+    
+    private RectTransform pauseRectTrans;
     private PlayerData _playerData;
+
+    private void Awake()
+    {
+        if(pausePanelGO != null)
+            pauseRectTrans = pausePanelGO.GetComponent<RectTransform>();
+    }
 
     private void Start()
     {
         _playerData = GameManager.Instance.FetchPlayerData();
+        pauseMenu.SetActive(false);
+        isPaused = false;
     }
 
     private void Update()
     {
         UpdateUIBar();
+
+        if (Input.GetKeyDown(KeyCode.Escape)|| Input.GetKeyDown(KeyCode.P))
+        {
+            if (!isPaused)
+            {
+                isPaused = true;
+                PauseGame();
+            }
+            else
+            {
+                isPaused = false;
+                UnpauseGame();
+            }
+
+        }
     }
 
     private void UpdateUIBar()
@@ -33,4 +70,32 @@ public class UIManager : MonoBehaviour
         foodText.text = _playerData.FoodSupply.ToString();
         oxygenText.text = _playerData.Oxygen.ToString("P0").Replace("%", "");
     }
+
+    #region Pause Panel
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        PausePanelIntro();
+    }
+
+    public async void UnpauseGame()
+    {
+        await PausePanelOutro();
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void PausePanelIntro()
+    {
+        pausefadePanel.DOFade(1, tweenDuration).SetUpdate(true);
+        pauseRectTrans.DOAnchorPosY(midY, tweenDuration).SetUpdate(true);
+    }
+
+    async Task PausePanelOutro()
+    {
+        pausefadePanel.DOFade(0, tweenDuration).SetUpdate(true);
+        await pauseRectTrans.DOAnchorPosY(topY, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+    }
+    #endregion
 }
